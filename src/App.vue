610d494b7064c56eb5a49c5541e6290f3cdfe875
simsoll <simsoll@users.noreply.github.com>
2016-11-29 20:51:43 +0100

    Added route change animations

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <navigation></navigation>
-        <router-view></router-view>
+        <hr>
+        <transition name="slide" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
@@ -18,5 +21,33 @@ export default {
 </script>
 
 <style>
+.slide-enter-active {
+    animation: slide-in 400ms ease-out forwards;
+}
 
+.slide-leave-active {
+    animation: slide-out 200ms ease-out forwards;    
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateY(-30px);
+        opacity: 0;
+    }
+    to {
+        transform: translateY(0);
+        opacity: 1;
+    }
+}
+
+@keyframes slide-out {
+    from {
+        transform: translateY(0);
+        opacity: 1;
+    }
+    to {
+        transform: translateY(-30px);
+        opacity: 0;
+    }
+}
 </style>
